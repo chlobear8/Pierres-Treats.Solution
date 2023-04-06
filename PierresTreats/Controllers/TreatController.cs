fdc157d5ce47d2af96e27ab47ddc719af137aac7
Chloe ONeil <chloesporri@yahoo.com>
2023-04-06 09:35:50 -0700

    add Details route for Treats

--- PierresTreats/Controllers/TreatController.cs
+++ PierresTreats/Controllers/TreatController.cs
@@ -35,5 +35,14 @@ namespace PierresTreats.Controllers
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
+
+    public ActionResult Details(int id)
+    {
+      Treat thisTreat = _db.Treats
+                      .Include(treat => treat.JoinEntities)
+                      .ThenInclude(join => join.Flavor)
+                      .FirstOrDefault(treat => treat.TreadId == id);
+      return View(thisTreat);        
+    }
   }
 }
