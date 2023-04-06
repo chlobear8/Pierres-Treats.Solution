4aa7cf79a9144bb5b1cbf19974a079e446f0f66f
Chloe ONeil <chloesporri@yahoo.com>
2023-04-06 09:12:51 -0700

    add HomeController

---
+++ PierresTreats/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using PierresTreats.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using System.Security.Claims;
+
+namespace PierresTreats.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly TreatsContext _db;
+    private readonly UserManager<ApplicationUser> _userManager;
+
+    public HomeController(UserManager<ApplicationUser> userManager, TreatsContext db)
+    {
+        _userManager = userManager;
+        _db = db;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    
+}
