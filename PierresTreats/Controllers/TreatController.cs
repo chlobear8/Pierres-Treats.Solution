6b09e844af2f7be878f63c69181ac9087e52084b
Chloe ONeil <chloesporri@yahoo.com>
2023-04-08 13:15:58 -0700

    BUGFIX indexes work

--- PierresTreats/Controllers/TreatController.cs
+++ PierresTreats/Controllers/TreatController.cs
@@ -4,13 +4,14 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
-// using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authorization;
 // using Microsoft.AspNetCore.Identity;
 // using System.Threading.Tasks;
 // using System.Security.Claims;
 
 namespace PierresTreats.Controllers
 {
+  [Authorize]
   public class TreatController : Controller
   {
     private readonly TreatsContext _db;
